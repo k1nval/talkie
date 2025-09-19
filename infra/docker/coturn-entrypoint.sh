b3bdfef6ac9dbe1a90761ef37b3d17e0bc3511bd
Stanislav <yunovichso@gmail.com>
2025-09-19 21:00:53 +0200

    Try another auth.

--- infra/docker/coturn-entrypoint.sh
+++ infra/docker/coturn-entrypoint.sh
@@ -24,7 +24,8 @@ exec turnserver \
   --realm=turn.talkie.k1nval.com \
   --server-name=turn.talkie.k1nval.com \
   --lt-cred-mech \
-  --user=turnuser:turn456 \
+  --use-auth-secret \
+  --static-auth-secret="${LIVEKIT_API_SECRET}" \
   --listening-port=3478 \
   --min-port=29000 \
   --max-port=29200 \
